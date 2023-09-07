cbd47253802e93c0e6f4cc57ad5a9febda4a912e
Azoozksa123 <asxd12341234@gmail.com>
2023-09-07 05:58:17 -0700

    f

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	/**
+	* main do excutring
+	*/
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	return (0);
+}
