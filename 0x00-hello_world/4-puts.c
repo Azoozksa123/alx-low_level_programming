fa887a73c54641c47c2a70ddfb5d7320e4209d0d
Azoozksa123 <asxd12341234@gmail.com>
2023-09-07 05:40:53 -0700

    da

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*puts funtcion for print
+*return: 1 if it good and 0 otherwise
+*main - main for excutrion
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
