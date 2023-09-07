eaadb2cdc2a2a7e9a935c2193ee3a19767e4ce48
Azoozksa123 <asxd12341234@gmail.com>
2023-09-07 06:00:47 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - Entry point
+* main - Entry point
 *
-* Return: Always 0 (success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	/**
-	* main do excutring
-	*/
-	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
