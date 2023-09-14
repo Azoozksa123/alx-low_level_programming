f59e5632ba248663879c0a0bf38a3eebebc1605e
Azoozksa123 <asxd12341234@gmail.com>
2023-09-14 02:25:44 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @a: the character to be multiplies
+* @b: the second charcter ot bemultiplies
+* Return: return the multiplies
+ */
+	void print_numbers(void)
+	{
+	int i = 0;
+	while(i < 10)
+	{
+	_putchar (i + '0');
+	i++;
+	}
+	_putchar ('\n');
+	}
