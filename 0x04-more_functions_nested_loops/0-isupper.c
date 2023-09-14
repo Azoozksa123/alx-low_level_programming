7165eee391aea2ef5dfb21b7be6db7a62f3c1e90
Azoozksa123 <asxd12341234@gmail.com>
2023-09-14 01:43:01 -0700

    isuppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isuppder - check if char is upercase
+* @c: is the char to be checked
+* Return: 1 if char is uppercase, otherwise 0.
+*/
+int _isupper(int c)
+{
+	if ((c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
