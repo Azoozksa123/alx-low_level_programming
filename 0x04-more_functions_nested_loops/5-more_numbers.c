7061901deadf33f87c020d655b7efa5b4af20797
Azoozksa123 <asxd12341234@gmail.com>
2023-09-14 03:21:52 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* more_numbers - function that print from 0 to 14 10 times
+* Return: return the multiplies
+*/
+
+	void more_numbers(void)
+	{
+	int i = 0;
+	int g = 0;
+
+	while (g < 11)
+	{
+
+		while (i < 15)
+		{
+			_putchar (i % 10 + '0');
+			i++;
+		}
+		_putchar ('\n');
+		g++;
+		i = 0;
+	}
+	}
