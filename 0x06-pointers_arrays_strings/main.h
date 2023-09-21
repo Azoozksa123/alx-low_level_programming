11669208487a8dc25ec41325b583d69df9fc5581
Azoozksa123 <89302625+Azoozksa123@users.noreply.github.com>
2023-09-21 21:21:18 +0800

    Done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *leet(char *);
+char *cap_string(char *);
 #endif
