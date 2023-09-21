dec15c2e168ce5afb257937bf726bd533b8db19d
Azoozksa123 <89302625+Azoozksa123@users.noreply.github.com>
2023-09-21 19:16:24 +0800

    aaa

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
 * *_strcat - This function appends the src string to the dest string.
-* @*dest: the string
-* @src the return xD
+* dest: the string
+* src the return xD
 * Return: the ip location of the string
 */
 char *_strcat(char *dest, char *src)
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 	size_t dest_len = strlen(dest);
 	size_t i;
 
-	for(i = 0 ; src[i] != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[dest_len + i] = src[i];
 
 	dest[dest_len + i] = '\0';
