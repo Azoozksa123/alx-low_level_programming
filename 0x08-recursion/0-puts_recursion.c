21dc15a868ca86024769d390aaef6f91f12bb42a
Azoozksa123 <89302625+Azoozksa123@users.noreply.github.com>
2023-09-28 22:04:56 +0800

    First

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * Return: changed array with new value for n bytes
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(*s + 1);
+	}
+	
+	else
+	_putchar('\n');
+		
+}
